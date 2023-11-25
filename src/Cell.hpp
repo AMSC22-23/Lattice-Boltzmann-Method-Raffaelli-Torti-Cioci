8c09a66266557e3aac83efdd3cb1a0bf8715947c
martinaraffaelli <martina.raffaelli@mail.polimi.it>
2023-11-25 09:15:15 +0000

    Added: computeOpposite function and Cell boundary

--- src/Cell.hpp
+++ src/Cell.hpp
@@ -26,6 +26,13 @@ class Cell
     float momX, momY;                         // Momentum density
     float rho;                                // Macroscopic density
     const std::vector<unsigned int> position; // cell position in the lattice
+    const int boundary[2];                    // array of two elements that describes the boundary of the cell
 };
+//boundary is an array of two elements; eache element can be 0, 1 or -1.
+//if we have a boundary {-1, 1} it means that we can't go in the "opposite direction of the x axis" (not on left)
+//and we can't go in the direction of the y axis (not up).
+//sum up: the first element of the vector tells us which direction on x we can't follow in that cell and
+//the second element tells us which direction on y we can't follow. If one of the element is 0 it means that
+//we have no boundary in this direction
 
 #endif // CELL_HPP
