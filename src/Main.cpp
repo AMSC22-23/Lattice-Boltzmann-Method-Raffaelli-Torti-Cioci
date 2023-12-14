a1067db9b36fbd2a2035852c9fe38f3b32242885
MarcoCioci <marco.cioci@mail.polimi.it>
2023-12-14 12:37:48 +0100

    first commit of restart branch

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,32 +20,6 @@ int main(int argc, char const *argv[])
     float deltaTime = stof(argv[2]);
     int timeSteps = stoi(argv[3]);
 
-    // create lattice
-    Lattice lattice(filename);
-
-    // open output file
-    ofstream file;
-    file.open("output.txt");
-    if (!file.is_open())
-    {
-        throw runtime_error("Could not open file");
-    }
-
-    // write dimensions in first line
-    for (int i = 0; i < lattice.getShape().size(); i++)
-    {
-        file << lattice.getShape().at(i) << ' ';
-    }
-    file << '\n';
-
-    // update lattice for timeSteps
-    for (int i = 0; i < timeSteps; i++)
-    {
-        lattice.update(deltaTime, file);
-    }
-
-    // close output file
-    file.close();
 
     return 0;
 }
