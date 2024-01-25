80f6130dcc01fca307168c0869b76072f863a2b0
AndreaTorti-01 <eliandrea.yt@gmail.com>
2024-01-25 14:27:26 +0100

    corrected run shell script

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Check if two parameters are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./run.sh <data_file> <number>"
+# Check if at least two parameters are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: ./run.sh <data_file> <number_of_frames> [-gpu]"
     exit 1
 fi
 
 mkdir -p outputs
 
 # Use the parameters in the script
-./build/lbm "data/$1.txt" $2
+if [ "$3" == "-gpu" ]; then
+    ./build/lbm "data/$1.txt" $2 -gpu
+else
+    ./build/lbm "data/$1.txt" $2
+fi
+
 source env/bin/activate
 python scripts/plotting2D.py outputs/velocity_out.txt
